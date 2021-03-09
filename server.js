71b76ac8ad48a6a5258a52971349eeb4250f583c
Teresa Flaherty <teresaflaherty22@gmail.com>
2021-03-09 14:49:38 +0000

    Small fixes and styling

--- server.js
+++ server.js
@@ -8,35 +8,32 @@ app.use('/style', express.static(__dirname + '/style'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 io.on('connection', (socket) => {
+    console.log('Connection made')
     socket.name = 'Anonymous'
     socket.on('message', (msg, emotional_matrix) => {
+        console.log(`User Message: ${msg}`)
         data = {
             'sender_id': socket.name,
             'message': msg,
             'emotional_matrix': emotional_matrix
         }
         io.emit('message', { 'name': socket.name, 'message': msg });
-        try {
-            fetch('http://localhost:5005/webhooks/rest/webhook', 
-            {
-                method: 'POST',
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(res => {
-                if (res.length > 0) {
-                    response = res[0]['text'];
-                }
-                else {
-                    response = 'Sorry, I didn\'t understand that. Can you try rephrasing?';
-                }
-                io.emit('message', { 'name': 'RasaBot', 'message': response});
-            })
-        }
-        catch (error) {
-            console.error(error)
-            io.emit('message', {'name': 'RasaBot', 'message': 'Sorry, Rasa is unavailable right now. Please try again later.'})
-        }
+        fetch('http://localhost:5005/webhooks/rest/webhook', 
+        {
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(res => {
+            if (res.length > 0) {
+                response = res[0]['text'];
+            }
+            else {
+                response = 'Sorry, I didn\'t understand that. Can you try rephrasing?';
+            }
+            console.log(`Bot Response: ${response}`)
+            io.emit('message', { 'name': 'RasaBot', 'message': response});
+        })
     })
     socket.on('join', (name) => {
         if (name != null && name != '') {
@@ -45,5 +42,5 @@ io.on('connection', (socket) => {
     })
 })
 
-http.listen(3000, () => console.log('Listening on port 3000!'))
+http.listen(3000, () => console.log('Listening on port 3000'))
 
